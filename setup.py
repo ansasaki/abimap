58df6cfe1e0ecf414093daaecfc29a3432083248
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2018-08-08 20:06:00 +0200

    setup.py: Add missing requirement for setuptools
    setuptools is required not only in build time, but also in runtime.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = ['setuptools']
 
 setup_requirements = ['pytest-runner']
 
